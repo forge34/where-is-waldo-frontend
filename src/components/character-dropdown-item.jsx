d45dcb4b27d476b103f0323766ea72bd4ab9f0f0
Forge <>
2024-07-11 02:01:38 +0300

    feat : now adds a marker when a character is found

--- src/components/character-dropdown-item.jsx
+++ src/components/character-dropdown-item.jsx
@@ -1,9 +1,10 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import PropTypes from "prop-types";
 import { dropdownContext } from "../utils/character-dropdown-context";
 
 function CharacterDropDownItem({ name, imgSrc }) {
-  const { x, y } = useContext(dropdownContext);
+  const { x, y, setFoundCharacters } = useContext(dropdownContext);
+
   async function handleClick(e) {
     e.stopPropagation();
     let res = await fetch("http://localhost:3000/check", {
@@ -19,12 +20,25 @@ function CharacterDropDownItem({ name, imgSrc }) {
     });
 
     res = await res.json();
+
+    if (res.found) {
+      setFoundCharacters((draft) => {
+        if (!draft.includes(name)) {
+          draft.push({
+            x: res.x,
+            y: res.y,
+            name: name,
+          });
+        }
+      });
+    }
+
     console.log(res);
   }
 
   return (
     <div className="dropdown-item" onClick={handleClick}>
-      <img width={"64px"} height={"64px"} src={imgSrc} />
+      <img width={"48px"} height={"48px"} src={imgSrc} />
       <h3>{name}</h3>
     </div>
   );
--- src/components/game-scene.jsx
+++ src/components/game-scene.jsx
@@ -1,13 +1,14 @@
 import React, { useState } from "react";
 import CharacterDropdown from "./character-dropdown";
 import bg1 from "../assets/bg2.jpg";
-import Rectangle from "./rectangle";
 import { useImmer } from "use-immer";
 import { dropdownContext } from "../utils/character-dropdown-context";
+import checkmark from "../assets/check-mark.svg";
 
 function GameScene({}) {
   const [open, setOpen] = useState(false);
   const [dimension, setDimension] = useImmer({ x: 0, y: 0 });
+  const [foundCharacters, setFoundCharacters] = useImmer([]);
 
   async function imageClick(e) {
     setDimension((draft) => {
@@ -19,10 +20,33 @@ function GameScene({}) {
 
   return (
     <div>
-      <dropdownContext.Provider value={{ x: dimension.x, y: dimension.y }}>
+      {foundCharacters.map((character) => {
+        return (
+          <img
+            width={32}
+            height={32}
+            key={character.name}
+            src={checkmark}
+            className="checkmark"
+            style={{
+              position: "absolute",
+              top: character.y,
+              left: character.x,
+            }}
+          />
+        );
+      })}
+
+      <dropdownContext.Provider
+        value={{
+          x: dimension.x,
+          y: dimension.y,
+          foundCharacters,
+          setFoundCharacters,
+        }}
+      >
         <CharacterDropdown isOpen={open} />
       </dropdownContext.Provider>
-      {open && <Rectangle x={dimension.x} y={dimension.y} />}
       <img src={bg1} onClick={imageClick} />
     </div>
   );
