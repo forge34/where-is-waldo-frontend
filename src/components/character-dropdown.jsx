0eab203e3911cef61855c27e6af7c857675714f2
Forge <>
2024-07-11 02:00:50 +0300

    feat : add 4 new characters

--- src/components/character-dropdown.jsx
+++ src/components/character-dropdown.jsx
@@ -3,12 +3,31 @@ import PropTypes from "prop-types";
 import leonardo from "../assets/ninge turtle.png";
 import CharacterDropdownItem from "./character-dropdown-item";
 import { dropdownContext } from "../utils/character-dropdown-context";
-// display: open ? "block" : "none",
-//
+import goku from "../assets/goku.png";
+import jasonVoorhees from "../assets/jason voorhees.png";
+import guitarGuy from "../assets/guiater-guy.png";
+import mojoJojo from "../assets/mojo jojo.png";
+
 const dropdownItems = [
   {
     name: "leonardo",
     imgSrc: leonardo,
+  },
+  {
+    name: "goku",
+    imgSrc: goku,
+  },
+  {
+    name: "jason voorhees",
+    imgSrc: jasonVoorhees,
+  },
+  {
+    name: "guitar guy",
+    imgSrc: guitarGuy,
+  },
+  {
+    name: "mojo jojo",
+    imgSrc: mojoJojo,
   },
 ];
 
