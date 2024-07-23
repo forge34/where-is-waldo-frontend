98dd41c68d10f92f933a3e76a3da4938120fc174
forge34 <28455546+forge34@users.noreply.github.com>
2024-07-23 03:42:32 +0300

    feat : add select game page to index

--- src/index.jsx
+++ src/index.jsx
@@ -1,17 +1,39 @@
 import React, { useState } from "react";
 import Header from "./components/header";
 import Timer from "./components/timer";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
+import bg1 from "./assets/bg2.jpg";
 
 function Index({}) {
   const [userTime, setUserTime] = useState(0);
-  const [stopTimer, setStopTimer] = useState(false);
+  const [stopTimer, setStopTimer] = useState(true);
+  const localtion = useLocation();
+  const navigate = useNavigate();
   return (
     <div>
       <Header>
         <Timer setExternalTime={setUserTime} stopTimer={stopTimer}></Timer>
       </Header>
-      <Outlet context={[userTime, setStopTimer]}></Outlet>
+      {localtion.pathname === "/" ? (
+        <div className="index">
+          <div className="game-card">
+            <div className="card-image">
+              <img src={bg1} />
+            </div>
+            <button
+              className="card-bottom"
+              onClick={() => {
+                setStopTimer(false);
+                navigate("/game");
+              }}
+            >
+              Start game
+            </button>
+          </div>
+        </div>
+      ) : (
+        <Outlet context={[userTime, setStopTimer]}></Outlet>
+      )}
     </div>
   );
 }
