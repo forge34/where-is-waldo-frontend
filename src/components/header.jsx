908db0ef3baf8033af3e7bf741396ec04cc6be13
forge34 <28455546+forge34@users.noreply.github.com>
2024-07-23 21:04:37 +0300

    feat : improve general styling

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styles from "../styles/css/header.module.css";
+import logo from "../assets/logo.png";
 
 function Header({ children }) {
   return (
     <div className={styles.container}>
+      {/* <img src={logo} className={styles.logo} /> */}
       <p className={styles.title}>Hidden Heroes: Beyond the Veil</p>
       {children}
       <p className={styles.leaderboard}>Leaderboard</p>
